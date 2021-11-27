142f234844fed14d959bc2a36ecf150947d34309
Adriana <20183L101020@utcv.edu.mx>
2021-11-26 19:56:57 -0600

    Subiendo archivo de desempeño de segundo parcial

---
+++ src/js/app.js
@@ -0,0 +1,62 @@
+/* Declaracion de variables globales */
+let MAIN;
+let MODAL_POST;
+let BTN_SHOW_POST;
+let BTN_CANCEL_POST;
+let deferredPrompt;
+
+// Funciones
+const showPostModal = () => {
+  MAIN.style.display = 'none';
+  MODAL_POST.style.display = 'block';
+  setTimeout(() => {
+    MODAL_POST.style.transform = 'translateY(0)';
+  }, 1);
+};
+
+const closePostModal = () => {
+  MAIN.style.display = 'block';
+  MODAL_POST.style.transform = 'translateY(100vh)';
+};
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+});
+
+// Cuando se cargue todo nuestro DOM
+window.addEventListener('load', async() => {
+  MAIN = document.querySelector('#main');
+  MODAL_POST = document.querySelector('#modal-post-section');
+  BTN_SHOW_POST = document.querySelector('#btn-upload-post');
+  BTN_SHOW_POST.addEventListener('click', showPostModal);
+  BTN_CANCEL_POST = document.querySelector('#btn-post-cancel');
+  BTN_CANCEL_POST.addEventListener('click', closePostModal)
+
+await Notification.requestPermission();
+
+if('serviceWorker' in navigator){
+
+const response=await navigator.serviceWorker.register('sw.js');
+
+if (response){
+  const ready=await navigator.serviceWorker.ready;
+  ready.showNotification('Hola usuario',{
+    body:'Este mensaje sonará',
+    vibrate:[200,100,200,100,200,100,200]
+  })
+ 
+}
+}
+
+const bannerInstall = document.querySelector('#banner-install');
+    bannerInstall.addEventListener('click', async () => {
+      if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const response = await deferredPrompt.userChoice;
+        if (response.outcome === 'dismissed') {
+          console.error('El usuario canceló la instalación');
+        }
+      }
+    });
+});
